9f63c653f80469fe23cbc7718de006e1b96aaf16
NG Chester <zhentingchester.ng@murex.com>
2019-07-25 22:38:29 +1000

    Essentially illustrating how the Customer constructor can inherit methods in the Person prototype but override its behavior (greeting method)

--- prototypal_inheritance.js
+++ prototypal_inheritance.js
@@ -9,5 +9,29 @@ Person.prototype.greeting = function () {
 	return `Hello there ${this.firstName} ${this.lastName}`
 }
 
-const person1 = new Person('John', 'Doe');
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+	Person.call(this, firstName, lastName);
+	this.phone = phone;
+	this.membership = membership;
+}
 
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Customer greeting
+Customer.prototype.greeting = function () {
+	return `Hello there ${this.firstName} ${this.lastName} welcome to our company`
+}
+
+const person1 = new Person('John', 'Doe');
+console.log(person1);
+console.log(person1.greeting());
+
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+console.log(customer1.greeting()); 
