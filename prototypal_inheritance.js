572eee33d612a32b2dd457bbcce0c0ad670b0516
NG Chester <zhentingchester.ng@murex.com>
2019-07-25 16:56:30 +1000

    Started study on prototypal inheritance. First, create a simple Person constructor and add a greeting method to the Persons prototype object

---
+++ prototypal_inheritance.js
@@ -0,0 +1,13 @@
+// Person constructor
+function Person(firstName, lastName) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+}
+
+// Greeting
+Person.prototype.greeting = function () {
+	return `Hello there ${this.firstName} ${this.lastName}`
+}
+
+const person1 = new Person('John', 'Doe');
+
